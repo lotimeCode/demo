08e28f0c4db5be2a43b41e58c5e09db347071083
wangzhimin <wangzhimin@corp.netease.com>
2018-09-26 10:49:12 +0800

    update

---
+++ src/main/java/com/elasticsearch/esdemo/demo/TestLog.java
@@ -0,0 +1,17 @@
+package com.elasticsearch.esdemo.demo;
+
+/**
+ * @author wangzhimin
+ * @version create 2018/9/26 10:48
+ */
+public class TestLog {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
